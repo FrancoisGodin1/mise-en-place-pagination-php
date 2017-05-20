1ecefbda7f2b0ef2ffefefdd845c360b5d0e528e
François Godin <francois.h.godin@gmail.com>
2017-05-20 18:16:34 +0200

    Update Livre.php

--- Livre.php
+++ Livre.php
@@ -3,20 +3,24 @@
 class Livre extends CI_Controller {
 
     // Seule la où les fonction(s) concernée(s) sont affichée(s)
+    // Il s'agit de faire évoluer la méthode index de manière à ce qu'elle tienne comte de la pagination
+    // codeIgniter propose une bibliothèque et une classe permettant de mettre en oeuvre cette pagination
+    // il faut donc au préalable charger le librairie 'pagination' dans le constructeur 
     
     function index() {
-        $config['base_url'] = site_url() . '/Livre/index/page';
-        $config['total_rows'] = $this->livreModel->get_count();
-        $config['per_page'] = 10;
-        $config['uri_segment'] = 4;
-        $config['use_page_numbers'] = TRUE;
-        $this->pagination->initialize($config);
-        $page = $this->uri->segment(4, 0);
+        // préparation de la pagination
+        $config['base_url'] = site_url() . '/Livre/index/page'; // ajoute la page de pagination dans l'uri
+        $config['total_rows'] = $this->livreModel->get_count(); //récupere le nb de livres total et l'initialise dans la pagination
+        $config['per_page'] = 10; // nb de livres par pages
+        $config['uri_segment'] = 4; // en quel position se trouve le numéro de page dans l'uri
+        $config['use_page_numbers'] = TRUE; 
+        $this->pagination->initialize($config); 
+        $page = $this->uri->segment(4, 0); // recup le numéro de page ou le rang du 1er livre
         if ($page > 0) {
             $page = $page * 10 - 10;
         }
         $data['livres'] = $this->livreModel->get_all_livres($page, $config['per_page']);
-        $links = $this->pagination->create_links();
+        $links = $this->pagination->create_links(); 
         $data['links'] = $links;
 
         //$data['livres']=$this->livreModel->get_all_livres();
